d5847079ecbfbea094bc4d7e614a6a061bf799a6
Sam Vipond <sammy.potato@gmail.com>
2017-01-31 15:24:23 -0500

    cleanup

--- functions.php
+++ functions.php
@@ -1,13 +1,13 @@
 <?php
 /**
- * piano-fund functions and definitions.
+ * piano_fund functions and definitions.
  *
  * @link https://developer.wordpress.org/themes/basics/theme-functions/
  *
- * @package piano-fund
+ * @package piano_fund
  */
 
-if ( ! function_exists( 'piano-fund_setup' ) ) :
+if ( ! function_exists( 'piano_fund_setup' ) ) :
 /**
  * Sets up theme defaults and registers support for various WordPress features.
  *
@@ -15,7 +15,7 @@ if ( ! function_exists( 'piano-fund_setup' ) ) :
  * runs before the init hook. The init hook is too late for some features, such
  * as indicating support for post thumbnails.
  */
-function piano-fund_setup() {
+function piano_fund_setup() {
 
 	/*
 	 * Make theme available for translation.
@@ -23,7 +23,7 @@ function piano-fund_setup() {
 	 * If you're building a theme based on Barry Livingston Music, use a find and replace
 	 * to change 'barry_livingstone_music' to the name of your theme in all the template files.
 	 */
-	load_theme_textdomain( 'piano-fund', get_template_directory() . '/languages' );
+	load_theme_textdomain( 'piano_fund', get_template_directory() . '/languages' );
 	
 	add_theme_support( 'title-tag' );
 
@@ -36,7 +36,7 @@ function piano-fund_setup() {
 
 	// This theme uses wp_nav_menu() in one location.
 	register_nav_menus( array(
-		'primary' => esc_html__( 'Primary', 'piano-fund' ),
+		'primary' => esc_html__( 'Primary', 'piano_fund' ),
 	) );
 
 	/*
@@ -64,13 +64,13 @@ function piano-fund_setup() {
 	) );
 
 	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'piano-fund_custom_background_args', array(
+	add_theme_support( 'custom-background', apply_filters( 'piano_fund_custom_background_args', array(
 		'default-color' => 'ffffff',
 		'default-image' => '',
 	) ) );
 }
 endif;
-add_action( 'after_setup_theme', 'piano-fund_setup' );
+add_action( 'after_setup_theme', 'piano_fund_setup' );
 
 
 
@@ -79,11 +79,11 @@ add_action( 'after_setup_theme', 'piano-fund_setup' );
  *
  * @link https://developer.wordpress.org/themes/functionality/sidebars/#registering-a-sidebar
  */
-function piano-fund_widgets_init() {
+function piano_fund_widgets_init() {
 	register_sidebar( array(
-		'name'          => esc_html__( 'Sidebar', 'piano-fund' ),
+		'name'          => esc_html__( 'Sidebar', 'piano_fund' ),
 		'id'            => 'sidebar-1',
-		'description'   => esc_html__( 'Add widgets here.', 'piano-fund' ),
+		'description'   => esc_html__( 'Add widgets here.', 'piano_fund' ),
 		'before_widget' => '<section id="%1$s" class="widget %2$s">',
 		'after_widget'  => '</section>',
 		'before_title'  => '<h2 class="widget-title">',
@@ -91,7 +91,7 @@ function piano-fund_widgets_init() {
 	) );
 	
 }
-add_action( 'widgets_init', 'piano-fund_widgets_init' );
+add_action( 'widgets_init', 'piano_fund_widgets_init' );
 
 
 
--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php get_header(); ?>
 
 <h1>Sammy's Starter Theme</h1>
-<button class="btn-danger" id="test">TEST</button>
+<button class="btn-danger" id="test">TESTing</button>
 
 <?php get_footer(); ?>
